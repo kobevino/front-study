ce6fe5554fdb7b3d8011f460ba25845f9a13af96
Jaesung Lee <jason0853@gmail.com>
2021-06-05 15:15:27 +0900

    Algorithm: maximum number of candle kit

--- src/algorithm/__tests__/candleKit.test.ts
+++ src/algorithm/__tests__/candleKit.test.ts
@@ -3,7 +3,7 @@ import { solution as getCandleKit } from '../candleKit';
 describe('candleKit', () => {
   const input1 = [15, 11, 12, 39, 100, 65, 37, 11000];
   const input2 = [1, 2, 3];
-  const input3 = [100, 22, 37];
+  const input3 = [6, 9];
 
   it(`gets number of all candle kits. (${input1})`, () => {
     expect(getCandleKit(input1)).toBe(7);
@@ -14,6 +14,6 @@ describe('candleKit', () => {
   });
 
   it(`gets number of all candle kits. (${input3})`, () => {
-    expect(getCandleKit(input3)).toBe(2);
+    expect(getCandleKit(input3)).toBe(1);
   });
 });
--- src/algorithm/candleKit.ts
+++ src/algorithm/candleKit.ts
@@ -1,31 +1,20 @@
 /**
- *
- * @description 촛불 키트 알고리즘
- *
- * 1. 우선 숫자 하나하나 split을 쪼개서 배열에 저장한다.
- * 2. 저장한 배열이 0이 될 때까지 Set 을 이용하여 중복되지 않게 이중배열을 형성한다.
- * 3. break 구문에서 빠져나와 촛불 키트 갯수를 구한다.
- *
+ * @description
+ * 촛불 키트 최대 개수 구하기
+ * 예외조건) 6, 9는 같이 사용할 수 있다.
  */
-export function solution(arr: number[]): number {
-  const result: string[] = arr.reduce(
-    (acc: string[], curr: number) => [...acc, ...String(curr).split('')],
-    []
-  );
+export function solution(ages: number[]): number {
+  const kit: number[] = new Array(9).fill(0);
 
-  let totalKit: Array<Array<string>> = [];
+  ages.forEach((age) => {
+    while (age > 0) {
+      let idx = age % 10;
+      if (idx === 9) idx = 6;
+      kit[idx]++;
+      age = Math.floor(age / 10);
+    }
+  });
+  kit[6] = Math.ceil(kit[6] / 2);
 
-  for (let i = result.length; i > 0; i--) {
-    if (!result.length) break; // 빈 배열이면 loop 중단하고 결과값으로 바로 도출
-
-    const set = new Set(result);
-
-    totalKit = set.size !== 0 ? [...totalKit, [...set]] : [...totalKit];
-
-    set.forEach((item) => {
-      const idx = result.findIndex((num) => num === item);
-      result.splice(idx, 1);
-    });
-  }
-  return totalKit.length;
+  return Math.max(...kit);
 }
